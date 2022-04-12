86e4137eda91f84872fe2742b273f23b1f019dd1
MinhyeSim <smine0032@gmail.com>
2022-04-12 13:54:44 +0900

    13:54

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
-import {Layout} from "./common";
+import {Layout} from "./common"
 import {wrapper} from '../redux/store'
+import Head from "next/head"
 
 const App = ({ Component, pageProps }) => {
-  return (
+  return (<>    
+    <Head>
+      <mata charset="utf-8"/>
+      <meta name="viewport" 
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Soccer App</title>
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+    </>
+  )
+  App.propTypyes= {
+    Component: PropTypyes.elementType.isReqyired,
+    pageProps: propTypyes.any.isReqyired
+  }
 }
 
 export default wrapper.withRedux(App);
